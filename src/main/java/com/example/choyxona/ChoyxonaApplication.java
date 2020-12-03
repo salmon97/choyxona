4a452c488f347bb131171da2b328d32fd44ea3ab
Salmon <salmon.muminov@gmail.com>
2020-12-03 18:19:29 +0500

    war

--- src/main/java/com/example/choyxona/ChoyxonaApplication.java
+++ src/main/java/com/example/choyxona/ChoyxonaApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChoyxonaApplication extends SpringBootServletInitializer {
+public class ChoyxonaApplication  {
+
     public static void main(String[] args) {
         SpringApplication.run(ChoyxonaApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(ChoyxonaApplication.class);
-    }
+//    extends SpringBootServletInitializer
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(ChoyxonaApplication.class);
+//    }
 }
